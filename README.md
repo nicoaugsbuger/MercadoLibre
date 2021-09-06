dcfb94d86a1e523df92cb0d04c0f8e66ca251e6f
Nicolas <nicoaugs@gmail.com>
2021-09-06 17:42:14 -0300

    cambios

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MercadoLibre
-Bla
+Colores
+Amarillo: #EAC926;
+Azul: #03264C;
+Celeste: #1259c3;
+Gris oscuro: #666;
+Gris claro: #ebebeb;
+Verde: #39b54a;
---
+++ app.js
@@ -0,0 +1,18 @@
+//Importamos el servidor, que es un modulo de ter, por eso hay que instalarlo con "npm install express"
+const express = require("express");
+//Instn el servidor, ahora app es un OBJETO serivodor 
+const app = express();
+//Importamos el modulo nativo de Node para maniplar las URIs (por eso no hay que instalarlo con npm)
+const path = require('path');
+//Creamos la const con la direccion absoluta del public 
+const public = path.resolve('./public')//const public = path.join(__dirname, './public')
+//a) Llamamos al metodo static del modulo express
+//b) Converimos la direccion absoluta en carpeta publica para que este dispponible para los navegadores 
+//c) "App.use" indica al servidor que tiene que usar dicha carpeta
+app.use(express.static(public));
+//ponemos a escuchar al servidor
+// Recibe dos parametros, el primero es el puerto, y el segundo es la funcion o lo que queremos que se ejecute
+app.listen(3000, () => {console.log('Servidor corriendo en el puerto http://localhost:3000')});
+//
+app.get('/', (req, res) => {res.sendFile(path.resolve('./views/index.html'))});
+app.get('/login', (req, res) => {res.sendFile(path.resolve('./views/login.html'))});
